a64e0b85ae106d61c1d5233898ef21af63a80479
Mabolita <valentina.panetta@davinci.edu.ar>
2020-10-18 04:09:02 -0300

    Mabol-MenuRancioCompleto

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    [SerializeField] private GameObject _checkPointText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_checkPointText.activeSelf) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _checkPointText.SetActive(false);
+            GameManager.Instance.SetNewCheckpoint(transform);
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Transform _spawnPoint;
+
+    private Transform _currentCheckpoint;
+
+    public Transform CurrentCheckPoint => _currentCheckpoint;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        _currentCheckpoint = _spawnPoint;
+    }
+
+    
+    
+
+    public void SetNewCheckpoint(Transform checkPoint)
+    {
+        _currentCheckpoint = checkPoint;
+    }
+}
