2be92c043f0415a43d8a2784373530334870467f
BangPae <56806821+BangPae@users.noreply.github.com>
2020-10-19 01:37:38 -0300

    Player + Camara

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash
+{
+    public LayerMask _lm;
+    public Transform _pl;
+    public Transform _camPivot;
+
+    public float _distance;
+
+    public Dash(LayerMask lm, float distance, Transform player,Transform camPivot)
+    {
+        _lm = lm;
+        _distance = distance;
+        _pl = player;
+        _camPivot = camPivot;
+    }
+
+    public void DashC()
+    {
+        RaycastHit hit = new RaycastHit();
+        Vector3 initialPos = _pl.position;
+        if (Physics.Raycast(_pl.position, _pl.forward, out hit, _distance, _lm))
+        {
+           _pl.position = hit.point - (_pl.forward / 2);
+        }
+        else
+        {
+            _pl.position += _pl.forward * _distance;
+        }
+        _camPivot.position = _pl.position;
+    }
+
+}
