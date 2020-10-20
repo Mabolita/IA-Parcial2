5c534016268d5238c351f2085027ec18316b8a87
BangPae <56806821+BangPae@users.noreply.github.com>
2020-10-20 16:10:15 -0300

    Roulette

---
+++ Assets/Scripts/IRoulette.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRoulette <TKey>
+{
+    TKey Run(Dictionary<TKey, int> dic);
+}
---
+++ Assets/Scripts/RouletteEjemplo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteEjemplo : MonoBehaviour, IRoulette<string>
+{
+    public string Run(Dictionary<string, int> dic)
+    {
+        int total = 0;
+        //para saber el porcentaje total
+        foreach (var item in dic)
+        {
+            total += item.Value;
+        }
+        //elejir un numero random para la aleotaridad
+        int random = Random.Range(0, total);
+        //verifica cual toco
+        foreach (var item in dic)
+        {
+            random = random - item.Value;
+            if (random < 0)
+            {
+                return item.Key;
+            }
+        }
+        return default;
+    }
+}
