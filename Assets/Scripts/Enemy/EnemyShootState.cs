5cf159cacd3bce6274aa9381781c6087bd9e448b
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-10-20 14:48:29 -0300

    FSM casi completa

---
+++ Assets/Scripts/Enemy/EnemyShootState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootState : EnemyState
+{
+    public EnemyShootState(StateMachine sm, EnemyAI enemy) : base(sm, enemy)
+    {
+    }
+
+
+}
