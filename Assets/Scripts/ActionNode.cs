3563d651982cace1299e8ca2692cdcabda0031a7
BangPae <56806821+BangPae@users.noreply.github.com>
2020-10-20 20:39:12 -0300

    arbol de decisiones

---
+++ Assets/Scripts/ActionNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionNode : Inode
+{
+    public delegate void myDelegate();
+    myDelegate _action;
+
+    public ActionNode(myDelegate action)
+    {
+        _action = action;
+    }
+    public void Execute()
+    {
+        _action();
+    }
+}
---
+++ Assets/Scripts/QuestionNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionNode : Inode
+{
+    public delegate bool myDelegate();
+    myDelegate _question;
+    Inode _trueNode;
+    Inode _falseNode;
+
+    public QuestionNode(myDelegate question, Inode tN, Inode fN)
+    {
+        _question = question;
+        _trueNode = tN;
+        _falseNode = fN;
+    }
+
+    public void Execute()
+    {
+        if (_question())
+        {
+            _trueNode.Execute();
+        }
+        else
+        {
+            _falseNode.Execute();   
+        }
+    }
+}
