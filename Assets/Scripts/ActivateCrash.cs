73d83a130f969ddf0579fc8dc76239ad4b00513e
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-10-27 01:25:43 -0300

    Aguante Crash (invector deleteado pa)

---
+++ Assets/Scripts/ActivateCrash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ActivateCrash : MonoBehaviour
+{
+    public GameObject aguanteCrash, credits, exitButton, randomButton;
+
+    public void Activate()
+    {
+        aguanteCrash.SetActive(!aguanteCrash.activeSelf);
+        credits.SetActive(!credits.activeSelf);
+        exitButton.SetActive(!exitButton.activeSelf);
+        randomButton.SetActive(!randomButton.activeSelf);
+    }
+}
