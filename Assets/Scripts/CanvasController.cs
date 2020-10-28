77e82e770c449e8368087b7e19d71f6cb79a89ed
BangPae <56806821+BangPae@users.noreply.github.com>
2020-10-27 23:09:10 -0300

    contador y tiempo

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI gearsText;
+    public int gearsCount;
+    public float seconds;
+    public float minute;
+    public float hours;
+
+    private void Start()
+    {
+        timeText.text = hours + ":" + minute + ":" + seconds;
+        gearsText.text = gearsCount.ToString();
+    }
+
+    private void Update()
+    {
+        seconds += Time.deltaTime;
+        if (seconds >= 60)
+        {
+            seconds = 0;
+            minute++;
+        }
+        if (minute >= 60)
+        {
+            minute = 0;
+            hours++;
+        }
+        timeText.text = hours + ":" + minute + ":" + (int)seconds;
+        gearsText.text = gearsCount.ToString();
+    }
+}
