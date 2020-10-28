7679f66477741038dfd10e28e6f2c78c13a80de9
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-10-27 21:04:43 -0300

    Spawner by lea

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public List<GameObject> platforms = new List<GameObject>();
+    public float maxSpawnRate;
+    public float distanceSpawn;
+    public float timeToSpawn;
+    public Transform platformTarget;
+
+    private void Update()
+    {
+        timeToSpawn++;
+        if (timeToSpawn > maxSpawnRate)
+        {
+            Spawn();
+            timeToSpawn = Random.Range(0, maxSpawnRate - distanceSpawn);
+        }
+    }
+
+    public void Spawn()
+    {
+        var randomPlatform = Random.Range(0, platforms.Count - 1);
+        Waypoints waypoints = Instantiate(platforms[randomPlatform], transform.position, Quaternion.identity).GetComponent<Waypoints>();
+        waypoints.target = platformTarget;
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Waypoints : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    private Vector3 start, end;
+    private bool _lookRight = false;
+    public bool isCarPlatform;
+    public PlatformSpawner platformSpawner;
+
+    private void Start()
+    {
+        if (target != null)
+        {
+            target.parent = null;
+            start = transform.position;
+            end = target.position;
+        }
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            if (transform.position == target.position)
+            {
+                Flip();
+            }
+        }
+    }
+
+    void Flip()
+    {
+        if (!isCarPlatform)
+        {
+            target.position = (target.position == start) ? end : start;
+            _lookRight = !_lookRight;
+            transform.Rotate(0, 180, 0);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
